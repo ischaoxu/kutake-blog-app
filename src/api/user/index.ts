3a98bdd86f05411dc1dc8f31076dc8e749296249
liuzhaoxu <1244453574@qq.com>
2023-06-09 08:32:43 +0800

    统一API-100%

--- src/api/user/index.ts
+++ src/api/user/index.ts
@@ -2,15 +2,15 @@
 
 import request from '@/utils/request'
 
-// import type {
+import type {
 
-//     loginFormData,
+    loginFormData,
 
-//     loginResponseData,
+    loginResponseData,
 
-//     userInfoReponseData,
+    userInfoReponseData,
 
-// } from './type'
+} from './type'
 
 //项目用户相关的请求地址
 
@@ -22,14 +22,14 @@ enum API {
   LOGOUT_URL = '/admin/acl/index/logout',
 }
 // //登录接口
-// export const reqLogin = (data: loginFormData) =>
-//     request.post<any, loginResponseData>(API.LOGIN_URL, data)
+export const reqLogin = (data: loginFormData) =>
+    request.post<any, loginResponseData>(API.LOGIN_URL, data)
 // //获取用户信息
 
-// export const reqUserInfo = () =>
+export const reqUserInfo = () =>
 
-//     request.get<any, userInfoReponseData>(API.USERINFO_URL)
+    request.get<any, userInfoReponseData>(API.USERINFO_URL)
 
 // //退出登录
 
-// export const reqLogout = () => request.post<any, any>(API.LOGOUT_URL)
+export const reqLogout = () => request.post<any, any>(API.LOGOUT_URL)
---
+++ src/api/user/type.ts
@@ -0,0 +1,14 @@
+
+// TODO  修改完善moudle
+export interface loginFormData {
+    key: string;
+    value: string;
+}
+export interface loginResponseData {
+    key: string;
+    value: string;
+}
+export interface userInfoReponseData {
+    key: string;
+    value: string;
+}
